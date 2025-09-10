aeab34980de61140be9d5062a71ec64262fb066a
Adityah669 <adithyasantosht@gmail.com>
2025-09-10 19:43:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,34 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "your-backend-rg"
+    storage_account_name = "yourstorageaccount"
+    container_name       = "tfstate"
+    key                  = "logicapp.terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
-      source  = "azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.40.0"
     }
   }
 }
+  }
+}
 provider "azurerm" {
   features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+}
 }
 resource "azurerm_logic_app_workflow" "res-0" {
   enabled                            = true
+  integration_service_environment_id = ""
   location                           = "northcentralus"
+  logic_app_integration_account_id   = ""
   name                               = "consumptionlogicapp"
   parameters = {
     "$connections" = "{\"visualstudioteamservices\":{\"connectionId\":\"/subscriptions/12486a52-70e3-4ae6-9f94-e6bacd147419/resourceGroups/demonewtest_group/providers/Microsoft.Web/connections/visualstudioteamservices-2\",\"connectionName\":\"visualstudioteamservices-2\",\"id\":\"/subscriptions/12486a52-70e3-4ae6-9f94-e6bacd147419/providers/Microsoft.Web/locations/northcentralus/managedApis/visualstudioteamservices\"}}"
@@ -67,4 +84,26 @@ resource "azurerm_logic_app_action_custom" "res-1" {
   })
   logic_app_id = azurerm_logic_app_workflow.res-0.id
   name         = "Create_a_work_item"
+}
+
+
+variable "client_id" {
+  type        = string
+  description = "Azure Service Principal Client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure Service Principal Client Secret"
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
 }
