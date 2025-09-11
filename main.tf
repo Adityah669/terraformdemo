1fb7ef9670b0ad61f082c86cca015d94af6237db
Adityah669 <adithyasantosht@gmail.com>
2025-09-11 20:06:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,41 +1,58 @@
-resource "azurerm_resource_group" "logicapp_rg" {
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
   name     = "logicapp-consumption-rg"
   location = "East US"
 }
 
-resource "azurerm_logic_app_workflow" "logicapp" {
-  name                = "basic-consumption-logicapp"
-  location            = azurerm_resource_group.logicapp_rg.location
-  resource_group_name = azurerm_resource_group.logicapp_rg.name
+resource "azurerm_template_deployment" "logicapp" {
+  name                = "logicapp-deployment"
+  resource_group_name = azurerm_resource_group.example.name
+  deployment_mode     = "Incremental"
 
-  definition = jsonencode({
-    "$schema" = "https://schema.management.azure.com/providers/Microsoft.Logic/schemas/2016-06-01/workflowdefinition.json#"
+  template_body = jsonencode({
+    "$schema" = "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#"
     "contentVersion" = "1.0.0.0"
-    "parameters" = {}
-    "triggers" = {
-      "manual" = {
-        "type" = "Request"
-        "kind" = "Http"
-        "inputs" = {
-          "schema" = {}
-        }
-      }
-    }
-    "actions" = {
-      "response" = {
-        "type" = "Response"
-        "inputs" = {
-          "statusCode" = 200
-          "body" = {
-            "message" = "Hello from your Consumption Logic App!"
+    "resources" = [
+      {
+        "type" = "Microsoft.Logic/workflows"
+        "apiVersion" = "2019-05-01"
+        "name" = "basic-consumption-logicapp"
+        "location" = azurerm_resource_group.example.location
+        "properties" = {
+          "definition" = {
+            "$schema" = "https://schema.management.azure.com/providers/Microsoft.Logic/schemas/2016-06-01/workflowdefinition.json#"
+            "contentVersion" = "1.0.0.0"
+            "parameters" = {}
+            "triggers" = {
+              "manual" = {
+                "type" = "Request"
+                "kind" = "Http"
+                "inputs" = {
+                  "schema" = {}
+                }
+              }
+            }
+            "actions" = {
+              "response" = {
+                "type" = "Response"
+                "inputs" = {
+                  "statusCode" = 200
+                  "body" = {
+                    "message" = "Hello from your Consumption Logic App!"
+                  }
+                }
+              }
+            }
+            "outputs" = {}
           }
+          "state" = "Enabled"
         }
       }
-    }
-    "outputs" = {}
+    ]
   })
 
-  tags = {
-    environment = "dev"
-  }
+  parameters = {}
 }
