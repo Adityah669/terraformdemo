cbf3431431561db74208d52f14cc96d96b967994
Adityah669 <adithyasantosht@gmail.com>
2025-09-11 20:03:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,41 @@
-resource "azurerm_resource_group" "example" {
-  name     = "newresourcegroupmain1"
-  location = "West Europe"
+resource "azurerm_resource_group" "logicapp_rg" {
+  name     = "logicapp-consumption-rg"
+  location = "East US"
+}
+
+resource "azurerm_logic_app_workflow" "logicapp" {
+  name                = "basic-consumption-logicapp"
+  location            = azurerm_resource_group.logicapp_rg.location
+  resource_group_name = azurerm_resource_group.logicapp_rg.name
+
+  definition = jsonencode({
+    "$schema" = "https://schema.management.azure.com/providers/Microsoft.Logic/schemas/2016-06-01/workflowdefinition.json#"
+    "contentVersion" = "1.0.0.0"
+    "parameters" = {}
+    "triggers" = {
+      "manual" = {
+        "type" = "Request"
+        "kind" = "Http"
+        "inputs" = {
+          "schema" = {}
+        }
+      }
+    }
+    "actions" = {
+      "response" = {
+        "type" = "Response"
+        "inputs" = {
+          "statusCode" = 200
+          "body" = {
+            "message" = "Hello from your Consumption Logic App!"
+          }
+        }
+      }
+    }
+    "outputs" = {}
+  })
+
+  tags = {
+    environment = "dev"
+  }
 }
