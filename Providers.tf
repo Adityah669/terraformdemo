a7255cee727aecf8a96a60228e4cd2878de028f4
Adityah669 <adithyasantosht@gmail.com>
2025-09-15 20:04:00 +0530

    Update Providers.tf

--- Providers.tf
+++ Providers.tf
@@ -10,7 +10,7 @@ terraform {
 provider "azurerm" {
   features {}
   client_id       = "4df05d97-7fd0-40fe-bbbf-fc0f0463a62c"
-  client_secret   = variables.client
+  client_secret   = var.client_secret
   tenant_id       = "16b3c013-d300-468d-ac64-7eda0820b6d3"
   subscription_id = "12486a52-70e3-4ae6-9f94-e6bacd147419"
 }
