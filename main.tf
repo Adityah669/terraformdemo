c29d2dc54c8108428ccabd0687d2b7a129f8a91c
Adityah669 <adithyasantosht@gmail.com>
2025-09-16 22:50:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,3 +21,14 @@ resource "azurerm_logic_app_action_http" "example" {
   method       = "GET"
   uri          = "https://github.com/Adityah669/"
 }
+resource "azurerm_storage_account" "example" {
+  name                     = "newstoragename123"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
