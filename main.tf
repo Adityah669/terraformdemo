a3220bf32e5317dddbf415e89883df8481832a89
Adityah669 <adithyasantosht@gmail.com>
2026-07-01 17:25:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,6 @@
 terraform {
   required_version = ">= 1.5"
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>4.0"
-    }
-  }
-}
 
 #----------------------------------------------------
 # Resource Group
