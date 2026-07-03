1c7c3056782b62a9e5f4f32fe61742ff646c2839
Adityah669 <adithyasantosht@gmail.com>
2026-07-03 19:04:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -94,7 +94,7 @@ resource "azurerm_logic_app_standard" "logicapp" {
   location                   = azurerm_resource_group.rg.location
   resource_group_name        = azurerm_resource_group.rg.name
   app_service_plan_id        = azurerm_service_plan.plan.id
-  storage_account_access_key = False
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
   storage_account_name       = azurerm_storage_account.storage.name
 
 
